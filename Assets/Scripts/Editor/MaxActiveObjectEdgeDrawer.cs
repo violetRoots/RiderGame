b89d4581157d8c3429ff637162368fd26d6069e1
violetRoots <SkyCrusherer@gmail.com>
2022-06-22 16:21:34 +0300

    [+] Gizmos: MaxActiveObjectEdgeDrawer

---
+++ Assets/Scripts/Editor/MaxActiveObjectEdgeDrawer.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using RiderGame.SO;
+
+namespace RiderGame.Editor
+{
+    public class MaxActiveObjectEdgeDrawer : MonoBehaviour
+    {
+#if UNITY_EDITOR
+        private const float XPointPos = 1000.0f;
+
+        private void OnDrawGizmos()
+        {
+            Vector3 left = new Vector3(-XPointPos, GameConfiguration.Instance.MaxActiveObjectPosition, 0);
+            Vector3 right = new Vector3(XPointPos, GameConfiguration.Instance.MaxActiveObjectPosition, 0);
+
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(left, right);
+        }
+#endif
+    }
+}
